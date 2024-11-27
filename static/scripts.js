f73e905034d5aee3e0d92c2f0f72c35290ec4e18
eichiarakaki <justanunusualperson@gmail.com>
2024-11-27 18:11:22 +0900

    adding forms & requesting system to the backend

--- static/scripts.js
+++ static/scripts.js
@@ -16,3 +16,57 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(error => console.error('Error fetching movies:', error));
 });
 
+document.getElementById('create-movie-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('create-title').value;
+    const isbn = document.getElementById('create-isbn').value;
+    const firstname = document.getElementById('create-firstname').value;
+    const lastname = document.getElementById('create-lastname').value;
+
+    // creating the JSON to request the data from the backend
+    fetch('/movies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            isbn: isbn,
+            director: {
+                firstname: firstname,
+                lastname: lastname
+            }
+        })
+    }).then(response => response.json())
+        .then(data => {
+            console.log('Movie Created:', data);
+        })
+});
+
+document.getElementById('update-movie-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = document.getElementById('update-id').value;
+    const title = document.getElementById('update-title').value;
+    const isbn = document.getElementById('update-isbn').value;
+    const firstname = document.getElementById('update-firstname').value;
+    const lastname = document.getElementById('update-lastname').value;
+
+    // creating the JSON to request the data from the backend
+    fetch(`/movies/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: title,
+            isbn: isbn,
+            director: {
+                firstname: firstname,
+                lastname: lastname
+            }
+        })
+    }).then(response => response.json())
+        .then(data => {
+            console.log('Movie Updated:', data);
+        }).catch(error => console.error('Error updating movie:', error));
+});
